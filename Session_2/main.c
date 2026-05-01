4eacc98e9154d0329d085d0be221cb41345ab661
unofficialoffice01-org <rohanunofficialoffice01@gmail.com>
2026-05-01 04:02:08 +0000

    Name change

--- Session_2/main.c
+++ Session_2/main.c
@@ -1,7 +1,24 @@
-int main(){
-    int a,b,c;
-    a=4;
-    b=5;    
-    c=a+b;
-    return c;
+#include <stdint.h>
+
+void main(void);
+void main(void)
+{
+
+  //TODO – GPIO LED Blinking
+  //1. Enable the GPIO Peripheral Clock
+
+  //2. Configure the GPIO Pin Mode
+
+  while(1){
+    
+    //3. Set the Output Data Bit to Turn ON the LED
+
+    //4. Insert a Software Delay
+
+    //5. Clear the Output Data Bit to Turn OFF the LED
+
+    //6. Insert a Software Delay
+
+  }
+
 }
--- Session_3/main.c
+++ Session_3/main.c
@@ -1,24 +1,7 @@
-#include <stdint.h>
-
-void main(void);
-void main(void)
-{
-
-  //TODO – GPIO LED Blinking
-  //1. Enable the GPIO Peripheral Clock
-
-  //2. Configure the GPIO Pin Mode
-
-  while(1){
-    
-    //3. Set the Output Data Bit to Turn ON the LED
-
-    //4. Insert a Software Delay
-
-    //5. Clear the Output Data Bit to Turn OFF the LED
-
-    //6. Insert a Software Delay
-
-  }
-
+int main(){
+    int a,b,c;
+    a=4;
+    b=5;    
+    c=a+b;
+    return c;
 }
